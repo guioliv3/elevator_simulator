ff839685fc43905ea48ad21c3596a0641eb0cad1
victor <victorcoutinho2006@gmail.com>
2026-05-10 22:04:49 -0300

    FrontEnd

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import path from "path";
 import { ElevatorService } from "./elevator/ElevatorService";
 import { createElevatorRoutes } from "./elevator/ElevatorRoutes";
 
 export function createApp(service: ElevatorService) {
   const app = express();
+
   app.use(express.json());
 
+  // 🔥 serve o front (HTML)
+  app.use(express.static(path.join(__dirname, "../public")));
+
+  // 🔥 API do elevador
   app.use("/api", createElevatorRoutes(service));
 
   return app;
